badceab4e8ede050e1ceb7ddf2df25e62aa09dde
Steven Carmichael <blekenbleu@gmail.com>
2026-01-29 06:24:13 -0500

    remove TPLinkSmartBulb and console prompts

--- Program.cs
+++ Program.cs
@@ -6,68 +6,41 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
-using System.IO.Ports;
-using TPLinkSmartDevices.Devices;
 
+// https://github.com/Peteri-git/simple-HttpServer/tree/master
 namespace HttpServer
 {
-    class Program
+    internal class Program
     {
         delegate void SimpleDelegate();
-        static SerialPort port = new SerialPort("COM4",115200, Parity.None, 8, StopBits.One);
-        static Dictionary<string, SimpleDelegate> dic = new Dictionary<string, SimpleDelegate>();
+        static readonly Dictionary<string, SimpleDelegate> dic = new Dictionary<string, SimpleDelegate>();
         static string path;
-        static TPLinkSmartBulb bulb = new TPLinkSmartBulb("192.168.1.5");
-        static void Main(string[] args)
+        static void Main(/*string[] args*/)
         {
-            Console.WriteLine("Zadejte IP");
-            string ip = Console.ReadLine();
-            Console.WriteLine("Zadejte Port");
-            string port = Console.ReadLine();
-            Console.WriteLine("Zadej cestu k složce");
-            path = Console.ReadLine();
-            Console.WriteLine();
+            // Console.WriteLine("Zadejte IP");
+            string ip = "127.0.0.1"; //  Console.ReadLine();
+            // Console.WriteLine("Zadejte Port");
+            string port = "7734";  Console.ReadLine();
+            // Console.WriteLine("Zadej cestu k složce");
+            path = "R:\\Temp";   // Console.ReadLine();
+            Console.WriteLine(ip+":"+port+" for "+path+" files\n");
+/*
             dic.Add("/On", new SimpleDelegate(On));
             dic.Add("/Blinking", Blinking);
             dic.Add("/Off", new SimpleDelegate(Off));
-            dic.Add("/Switch",new SimpleDelegate(Switch));
-            TcpListener server = new TcpListener(IPAddress.Parse(ip),Convert.ToInt32(port));
+            dic.Add("/Switch", new SimpleDelegate(Switch));
+ */
+            TcpListener server = new TcpListener(IPAddress.Parse(ip), Convert.ToInt32(port));
             server.Start();
             while (true)
             {
                 var client = server.AcceptTcpClient();
-                Thread th = new Thread(process);
+                Thread th = new Thread(Process);
                 th.Start(client);
             }
         }
-        static void On()
-        {
-            bulb.PoweredOn = true;
-        }
-        static void Off()
-        {
-            bulb.PoweredOn = false;
-        }
 
-        static void Switch()
-        {
-            if (bulb.PoweredOn == true)
-            {
-                bulb.PoweredOn = false;
-            }
-            else if (bulb.PoweredOn == false)
-            {
-                bulb.PoweredOn = true;
-            }
-        }
-        static void Blinking()
-        {
-            port.Open();
-            port.Write("2");
-            port.Close();
-        }
-        static void process(object param)
+        static void Process(object param)
         {
             try
             {
@@ -90,7 +63,7 @@ namespace HttpServer
                     if (string.IsNullOrWhiteSpace(line))
                         break;
                 }
-                string[] filePaths = Directory.GetFiles(path); 
+                string[] filePaths = Directory.GetFiles(path);
                 if (actionLine[0] == "POST")
                 {
                     char[] postData = new char[contentLength];
@@ -106,18 +79,18 @@ namespace HttpServer
                     }
                     foreach (var file in files)
                     {
-                        if (input[1]==file)
+                        if (input[1] == file)
                         {
                             foreach (var item in MimeTypes._mappings)
                             {
                                 if (file.EndsWith(item.Key))
-                                {    
-                                    if (File.Exists(path +"\\"+ file.ToString()))
+                                {
+                                    if (File.Exists(path + "\\" + file.ToString()))
                                     {
                                         sw.WriteLine("HTTP/1.1 200 OK");
                                         sw.WriteLine($"Content-Type:{item.Value}; charset=utf-8");
                                         sw.WriteLine();
-                                        byte[] filee = File.ReadAllBytes(path +"\\"+ file.ToString());
+                                        byte[] filee = File.ReadAllBytes(path + "\\" + file.ToString());
                                         sw.BaseStream.Write(filee, 0, filee.Length);
                                         sw.BaseStream.Flush();
                                     }
@@ -133,44 +106,48 @@ namespace HttpServer
                     sw.Flush();
                     client.Close();
                 }
-                foreach (var  fgt in dic)
+                /*                foreach (var fgt in dic)
+                                {
+                                    if (actionLine[1] == fgt.Key)
+                                    {
+                                        SimpleDelegate tmp = fgt.Value;
+                                        tmp();
+                                    }
+                                }
+                                var test = actionLine[1].Split('/');
+                                if (test[1] == "jas")
+                                {
+                                    bulb.Brightness = Convert.ToInt32(test[2]);
+                                }
+*/
+                                if (actionLine[1] == "/Menu" || actionLine[1] == "/")
+                
                 {
-                    if (actionLine[1] == fgt.Key)
-                    {
-                        SimpleDelegate tmp = fgt.Value;
-                        tmp();
-                    }
-                }
-                var test = actionLine[1].Split('/');
-                if (test[1] == "jas")
-                {
-                    bulb.Brightness = Convert.ToInt32(test[2]);
-                }
-                if (actionLine[1]=="/Menu")
-                {
-                    Menu(sw, sr, filePaths);
-                }
-                foreach (var item in MimeTypes._mappings)
-                {
-                    if (actionLine[1].EndsWith(item.Key))
-                    {
+                    Menu(sw, filePaths);
+                } else
+                        /*
+                                        foreach (var item in MimeTypes._mappings)
+                                        {
+                                            if (actionLine[1].EndsWith(item.Key))
+                                            {
+                         */
                         if (File.Exists(path + actionLine[1].ToString()))
-                        {
-                            sw.WriteLine("HTTP/1.1 200 OK");
-                            sw.WriteLine($"Content-Type:{item.Value}; charset=utf-8");
-                            sw.WriteLine();
-                            byte[] file = File.ReadAllBytes(path + actionLine[1].ToString());
-                            sw.BaseStream.Write(file, 0, file.Length);
-                            sw.BaseStream.Flush();
-                        }
-                        else
-                        {
-                            sw.WriteLine("HTTP/1.1 404 NOT FOUND");
-                            sw.WriteLine();
-                        }
-
-                    }
+                {
+                    sw.WriteLine("HTTP/1.1 200 OK");
+                    sw.WriteLine($"Content-Type:text/html; charset=utf-8");
+                    sw.WriteLine();
+                    byte[] file = File.ReadAllBytes(path + actionLine[1].ToString());
+                    sw.BaseStream.Write(file, 0, file.Length);
+                    sw.BaseStream.Flush();
                 }
+                else
+                {
+                    sw.WriteLine("HTTP/1.1 404 NOT FOUND");
+                    sw.WriteLine();
+                }
+
+//                    }
+//                }
                 sw.Flush();
                 client.Close();
             }
@@ -178,9 +155,9 @@ namespace HttpServer
             {
                 Console.WriteLine(e);
             }
-
         }
-        static void Menu(StreamWriter sw,StreamReader sr, string[] files)
+
+        static void Menu(StreamWriter sw, string[] files)
         {
             sw.WriteLine("HTTP/1.1 200 OK");
             sw.WriteLine($"Content-Type:text/html; charset=utf-8");
@@ -188,21 +165,20 @@ namespace HttpServer
             sw.WriteLine("<html>");
             sw.WriteLine("<head>");
             sw.WriteLine("</head>");
-            sw.WriteLine("<body>");
+            sw.WriteLine("<body><h2>HttpServer by TcpListener</h2>Enter filename from "+path);
             sw.WriteLine("  <form action=\"/\" method=\"POST\">");
-            sw.WriteLine("   <input type=\"text\" name=\"neco\" />");
-            sw.WriteLine("   <input type=\"submit\" value=\"odeslat\" />");
+            sw.WriteLine("   <input type=\"text\" name=\"something\" />");
+            sw.WriteLine("   <input type=\"submit\" value=\"send\" />");
             sw.WriteLine("  </form>");
             sw.WriteLine("<ul>");
             foreach (var item in files)
             {
-                string[] tmp= item.Split('\\');
-                sw.WriteLine($"<li>{item}</li><a href=\"{tmp.Last()}\">Otevřít</a>");
+                string[] tmp = item.Split('\\');
+                sw.WriteLine($"<li>{item}</li><a href=\"{tmp.Last()}\">Open</a>");
             }
             sw.WriteLine("</ul>");
             sw.WriteLine("</body>");
             sw.WriteLine("</html>");
         }
-       
     }
 }
---
+++ README.md
@@ -0,0 +1,3 @@
+## HttpServer using [TCPListener](https://learn.microsoft.com/en-us/dotnet/api/system.net.sockets.tcplistener?view=netframework-4.8)
+ &emsp; *from [simple-HttpServer](https://github.com/Peteri-git/simple-HttpServer)*  
+- .NET Framework 4.8
